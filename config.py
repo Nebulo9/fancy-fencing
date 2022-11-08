1b85c62807dff743fddd9425b8fb070c9c43e6c2
Mathéo DESSAAUVAGES <matdessauv@gmail.com>
2022-11-08 10:23:12 +0100

    Changing the content reading to take the entire content of the file

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ def load_scene(filename="default.ffscene"):
             with open(filename,"r") as instream:
                 content = instream.readline()
                 # Check if the content contains cells, positions of players and obstacles
-                if search(r"^(_|x)+|(1{1})|(2{1})$",content):
+                if search(r"^(_|x)+|1|2$",content):
                     length = len(content)
                     pos_p1 = content.index("1")
                     pos_p2 = content.index("2")
@@ -26,9 +26,9 @@ def load_player(filename):
         # Check if the file has the correct extension
         if search(r"\.ffplayer$",filename):
             with open(filename,"r") as instream:
-                content = instream.readline()
+                content = str(instream.readlines())
                 # Removing spaces, brackets, quotes from the content and splitting it to have lists containing a property and its value
-                splitted = [s.split(":") for s in sub(r"{|}|\s|\"","",content).split(",")]
+                splitted = [s.split(":") for s in sub(r"{|}|\s|\"|\[|\]|\'","",content).split(",")]
                 player = {}
                 # Mapping the properties and their values into a dictionnary
                 for l in splitted:
