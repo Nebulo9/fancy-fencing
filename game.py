63d853a5ac25714cb4a905be5afad5239b37409d
Mathéo DESSAAUVAGES <matdessauv@gmail.com>
2022-11-15 08:13:17 +0100

    made game a class

--- game.py
+++ game.py
@@ -1,52 +1,26 @@
 import window
 import curses
 
-def start_game(scene,player1,player2):
-    window.start_session()
-    pairs = [{"number": 1,"fg":curses.COLOR_WHITE,"bg":curses.COLOR_BLACK}]
-    game_win = window.create_window(pairs)
+class Game:
+    def __init__(self,scene,player1,player2):
+        self.__scene = scene
+        self.__player1 = player1
+        self.__player2 = player2
     
-    while True:
-        c = game_win.getch()
-        if c == ord('q'):
-            move_left(player1)
-        elif c == curses.KEY_LEFT:
-            move_left(player2)
-        elif c == ord('d'):
-            move_right(player1)
-        elif c == curses.KEY_RIGHT:
-            move_right(player2)
-        elif c == ord('a'):
-            jump_left(player1)
-        elif c == ord('l'):
-            jump_left(player2)
-        elif c == ord('e'):
-            jump_right(player1)
-        elif c == ord('m'):
-            jump_right(player2)
-        elif c == ord('z'):
-            make_attack(player1)
-        elif c == ord('o'):
-            make_attack(player2)
-        elif c == curses.KEY_ENTER:
-            window.end_session()
-            break
-    end_game()
+    def start(self):
+        pairs = [{"number": 1,"fg":curses.COLOR_WHITE,"bg":curses.COLOR_BLACK}]
+        self.__win = window.GWin(pairs)
+        self.__win.create()
+    
+    def end(self):
+        return True
 
-def end_game(scores=0):
-    return True
-
-def move_right(player):
-    return True
-
-def move_left(player):
-    return True
-
-def jump_left(player):
-    return True
-
-def jump_right(player):
-    return True
-
-def make_attack(player):
-    return True
+    @property
+    def scene(self):
+        return self.__scene
+    @property
+    def player1(self):
+        return self.__player1
+    @property
+    def player2(self):
+        return self.__player2
